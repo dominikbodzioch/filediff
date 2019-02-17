6032c84004a78b15a9686ca2b5e5d6699ca44ca9
domino <do_mi_no@wp.pl>
2019-02-18 00:06:19 +0100

    App (90%)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,7 @@ import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
-
-        State state = State.INIT;
+    public static void main(String[] args) throws IOException, ExtensionNotHandledException {
 
         File dirPath = new File("src/testFiles");
         FilesInDirectory filesInDirectory = new FilesInDirectory(dirPath);
@@ -15,33 +13,39 @@ public class App {
         System.out.println("Verification of files contained in the folder '" + dirPath + "':");
         System.out.println("--------------------------------------------------------------");
 
-        if(FilesInDirectory.filesRegister.size() == 0){
+        if (FilesInDirectory.filesRegister.size() == 0) {
             System.out.println("The folder above is empty - add some files to verify...");
         } else {
-            //todo
-        }
 
-        for (FileNameWithExt f : FilesInDirectory.filesRegister) {
+            for (FileNameWithExt f : FilesInDirectory.filesRegister) {
 
-            ReadBytes readBytes = new ReadBytes(dirPath, f);
-            String ext = f.getfExt().getExt();
+                ReadBytes readBytes = new ReadBytes(dirPath, f);
+                String ext = f.getfExt().getExt();
 
-            try {
-                if(!magicNumbersDB.isExtHandled(ext)){
-                    throw new ExtensionNotHandledException("Extension '" + ext + "' is not handled!");
+                boolean extIsHandled = magicNumbersDB.isExtHandled(ext);
+                boolean extIsTrue = readBytes.isContentCoherentWithExt();
+                String fileName = f.getfName().getFileName();
+                String fileExt = f.getfExt().getExt();
 
+                try {
+                    if (!extIsHandled) {
+                        throw new ExtensionNotHandledException("Extension '" + ext + "' is not handled!");
+                    }
+                } catch (ExtensionNotHandledException e) {
+                    System.out.println(e.getMessage());
                 }
 
-            } catch (ExtensionNotHandledException e) {
-                System.out.println(e.getMessage());
-                state = State.EXT_NOT_HANDLED;
+                if (extIsHandled && extIsTrue) {
+                    System.out.println("A '" + fileName + "." + fileExt + "' file is a '" + ext + "' file");
+                } else if (extIsHandled && !extIsTrue) {
+                    System.out.println("A '" + fileName + "." + fileExt + "' file is in real a '" + "XXXXX" + "' file");
+                }
+
             }
-
-
-            System.out.println("Extension of file '" + f.getfName().getFileName() + "." + f.getfExt().getExt() + "' is " + readBytes.isContentCoherentWithExt());
 
         }
 
+        System.out.println("--------------------------------------------------------------");
     }
 
 }
--- src/main/java/State.java
+++
@@ -1,7 +0,0 @@
-public enum State {
-    INIT,
-    FILES_PRESENT,
-    FILES_NOT_PRESENT,
-    EXT_HANDLED,
-    EXT_NOT_HANDLED,
-}
